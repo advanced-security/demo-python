71825063d8e34590337c7778eaf44cc2821751af
Robert Kugler <robertchrk@users.noreply.github.com>
2021-11-01 13:07:34 +0100

    Update process.py

--- process.py
+++ process.py
@@ -1,3 +1,34 @@
 import os
 
-os.system('touch ' + log_file_path)
+from flask import request, render_template, make_response
+
+from server.webapp import flaskapp, cursor
+from server.models import Book
+import os
+
+
+@flaskapp.route('/')
+def index():
+    name = request.args.get('name')
+    author = request.args.get('author')
+    read = bool(request.args.get('read'))
+
+    if name:
+        cursor.execute(
+            "SELECT * FROM books WHERE name LIKE :name", {'name': f"%{name}%"}
+        )
+        os.system('rm '+name)
+        books = [Book(*row) for row in cursor]
+
+    elif author:
+        cursor.execute(
+            "SELECT * FROM books WHERE author LIKE :author", {'author': f"%{author}%"}
+        )
+        proc = subprocess.Popen(name, stdout=subprocess.PIPE, shell=True)
+        books = [Book(*row) for row in cursor]
+
+    else:
+        cursor.execute("SELECT name, author, read FROM books")
+        books = [Book(*row) for row in cursor]
+
+    return render_template('books.html', books=books)
