399ba2d3361b0df3d3851f33caf129c36fd489b6
Jose Palafox <josepalafox@github.com>
2023-06-09 09:17:40 -0700

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -28,3 +28,26 @@ def index():
         books = [Book(*row) for row in cursor]
 
     return render_template('books.html', books=books)
+@flaskapp.route('/pizza')
+def index():
+    name = request.args.get('name')
+    author = request.args.get('author')
+    read = bool(request.args.get('read'))
+
+    if name:
+        cursor.execute(
+            "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
+        )
+        books = [Book(*row) for row in cursor]
+
+    elif author:
+        cursor.execute(
+            "SELECT * FROM books WHERE author LIKE '%" + author + "%'"
+        )
+        books = [Book(*row) for row in cursor]
+
+    else:
+        cursor.execute("SELECT name, author, read FROM books")
+        books = [Book(*row) for row in cursor]
+
+    return render_template('books.html', books=books)
