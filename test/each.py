d8e131567fcc9c3aa0ab2c2ae32b7485d241671d
sean-bts-cop <sean.alagar-mccartney@portlandoregon.gov>
2024-07-31 17:23:26 -0700

    Update each.py

--- test/each.py
+++ test/each.py
@@ -5,5 +5,8 @@ base64_secret = 'c2VjcmV0IG1lc3NhZ2Ugc28geW91J2xsIG5ldmVyIGd1ZXNzIG15IHBhc3N3b3J
 hex_secret = '8b1118b376c313ed420e5133ba91307817ed52c2'
 basic_auth = 'http://username:whywouldyouusehttpforpasswords@example.com'
 
-aws_access_key = 'AKIAIOSFODNN7EXAMPLE'
-aws_secret_access_key = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+aws_access_key = 'AKIA2UC3BSXMORT3VIE5'
+aws_secret_access_key = '60bUBMmiQLmaCXnagurDPnh6n0R30qcutkCXvBL5'
+
+canary_aws_access_key = 'AKIAIOSFODNN7EXAMPLE'
+canary_aws_secret_access_key = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
